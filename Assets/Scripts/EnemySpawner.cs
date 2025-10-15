c3dc9285e6bfc61016ab578b5bfa05148ff38e64
Joel <719325@student.dusd.net>
2025-10-15 12:41:46 -0700

    cat

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,24 +5,58 @@ public class EnemySpawner : MonoBehaviour
     public invader[] enemyPrefabs;
     public int row = 5;
     public int columns = 11;
+    public float spacing = 4f;
+
+    private Vector3 _direction = Vector2.right;
+    public float speed = 1f;
 
 
     void Awake()
     {
         for (int row = 0; row < this.row; row++ )
         {
-            float width = 2 * (this.columns - 1);
-            float height = 2 * (this.row - 1);
+            float width = spacing * (this.columns - 1);
+            float height = spacing * (this.row - 1);
             Vector2 centerOffset = new Vector2(-width / 2, -height / 2);
-            Vector3 rowPosition = new Vector3(centerOffset.x,centerOffset.y + (row * 2), 0);
+            Vector3 rowPosition = new Vector3(centerOffset.x,centerOffset.y + (row * spacing), 0);
             for (int col = 0; col < this.columns; col++)
             {
                 invader invader = Instantiate(this.enemyPrefabs[row], this.transform);
                 Vector3 position = rowPosition;
-                position.x += col * 2;
-                invader.transform.position = position;
+                position.x += col * spacing;
+                invader.transform.localPosition = position;
 
             }
         }
+    }
+
+    private void Update()
+    {
+        this.transform.position += _direction * this.speed * Time.deltaTime;
+
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+        foreach (Transform invader in this.transform)
+        {
+            if (!invader.gameObject.activeInHierarchy)
+            {
+                continue;
+            }
+            if (_direction == Vector3.right && invader.position.x > rightEdge.x)
+            {
+                AdvanceRow();
+            }
+            else if (_direction == Vector3.left && invader.position.x < leftEdge.x)
+            {
+                AdvanceRow();
+            }
+        }
+    }
+    void AdvanceRow()
+    {
+        _direction *= 1;
+        Vector3 position = this.transform.position;
+        position.y -= 1.0f;
+        this.transform.position = position;
     }
 }
--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class LevelGenerator : MonoBehaviour
 {
     public GameObject bunker;
+    public float distance = 8f;
 
     void Start()
     {
         for (int i = 0; i < 4; i++)
         {
-            Instantiate(bunker, new Vector3(Vector3.left.x + (i * 8)-(11), -10f, 0), Quaternion.identity);
+            Vector3 position = new Vector3(Vector3.left.x + ( (i * distance) - ( ((4-1.25f) * distance)/2) ), -10,0f);
+            Instantiate(bunker, position, Quaternion.identity);
         }
     }
 }
