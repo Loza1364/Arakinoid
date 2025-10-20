f70262d6c1b59d2938025e215e90b9e549afa4fc
Joel <719325@student.dusd.net>
2025-10-20 15:30:39 -0700

    stuff part 88

--- Assets/Scripts/enemyProjectile.cs
+++ Assets/Scripts/enemyProjectile.cs
@@ -30,4 +30,12 @@ public class enemyProjectile : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("bunker"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
